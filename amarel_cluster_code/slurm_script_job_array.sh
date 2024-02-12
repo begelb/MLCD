f06962fb14adafd864c721b93bd448f41d6683be
begelb <69121050+begelb@users.noreply.github.com>
2024-02-12 09:45:47 -0500

    Update slurm script

--- amarel_cluster_code/slurm_script_job_array.sh
+++ amarel_cluster_code/slurm_script_job_array.sh
@@ -3,8 +3,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=run_job_array   # Job name
-#SBATCH --output=system17_outputs/output_%N_%j.out  # STDOUT output file
-#SBATCH --error=system17_outputs/output_%N_%j.err   # STDERR output file (optional)
+#SBATCH --output=system1_outputs/output_%N_%j.out  # STDOUT output file
+#SBATCH --error=system1_outputs/output_%N_%j.err   # STDERR output file (optional)
 #SBATCH --partition=p_mischaik_1  # Partition (job queue)
 #SBATCH --nodes=1                  # Number of nodes
 #SBATCH --ntasks=1                 # Total number of tasks across all nodes
@@ -18,9 +18,9 @@
 # Load necessary modules
 # module purge
 # module load intel/19.0.3
-cd /home/bg545/Research
+cd /home/bg545/attractor_id/
 
 # Run python script with input data. The variable ${SLURM_ARRAY_TASK_ID}
 # is the array task id and varies from 0 to 99 in this example.
-srun python3 /home/bg545/Research/run_experiment.py ${SLURM_ARRAY_TASK_ID}
+srun python3 /home/bg545/attractor_id/run_experiment.py ${SLURM_ARRAY_TASK_ID}
 # /home/bg545/Research/Final_Reg_Training_Homology_colormap.py
