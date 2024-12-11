1feb158faddcaa8cd9a8544c55b56927d9ca96fc
begelb <69121050+begelb@users.noreply.github.com>
2024-12-11 16:08:02 -0500

    Improved efficiency in forward pass of cubical network

--- src/attractor_id/network.py
+++ src/attractor_id/network.py
@@ -16,42 +16,38 @@ def get_optimizer(config, model):
 class Regression_Cubical_Network_One_Nonlinearity(nn.Module):
     def __init__(self, N, batch_size, config):
         super(Regression_Cubical_Network_One_Nonlinearity, self).__init__()
-        
-        # d is the dimension of the input
-        self.d = config.dimension
-        self.num_labels = config.num_labels
-        data_bounds_list = config.data_bounds
         # N is the width of the first hidden layer
         self.N = N
-        self.batch_size = batch_size
+        # d is the dimension of the input
+        d = config.dimension
+        self.group_size = N//d
+        num_labels = config.num_labels
+        data_bounds_list = config.data_bounds
         self.threshold_prediction = config.threshold_prediction
-        weak_weight_share = config.weak_weight_share
-
+        self.threshold = float(num_labels - 1)
 
         # shared_weight_matrix is a d x d matrix with rows made up of the weights that are each shared up to a constant multiple (weight_cofficients) among a set of N//d nodes of the hidden layer
-        # shared_weight_matrix is an initialized as a d x d identity matrix
-        self.shared_weight_matrix =  nn.Parameter(torch.eye(self.d), requires_grad=True)
+        # shared_weight_matrix is initialized as a d x d identity matrix
+        self.shared_weight_matrix =  nn.Parameter(torch.eye(d), requires_grad=True)
         # register shared_weight_matrix so that it can be accessed using this name at any point
         self.register_parameter('shared_weight_matrix', self.shared_weight_matrix)
 
         # weight coefficients are the coefficients that relate the hidden layer weights to the shared_weight_matrix
         # there is one coefficient for each node in the hidden layer
-
         # if weak_weight_share = True, then the network has trainable weight parameters, so requires_grad below is set to True
-        self.weight_coefficients = nn.Parameter(torch.ones(self.N), requires_grad = weak_weight_share)
-        #self.weight_coefficients = nn.Parameter(torch.rand(self.N) + 1, requires_grad = True)
+        self.weight_coefficients = nn.Parameter(torch.ones(self.N), requires_grad = config.weak_weight_share)
         self.register_parameter('weight_coefficients', self.weight_coefficients)
 
         # assign each node in the hidden layer a key between 0 and d - 1, with N // d nodes being assigned each key
         hidden_node_keys = torch.zeros(N, dtype=torch.long)
-        group_size = N // self.d
-        for i in range(self.d):
+        group_size = N // d
+        for i in range(d):
             hidden_node_keys[i*group_size:(i+1)*group_size] = i
 
         # initialize the biases of the hidden nodes of key = i via a uniform distribution over the interval that makes up the data domain in i-th dimension
         bias_initial_lower_bounds = torch.zeros(N)
         bias_initial_upper_bounds = torch.zeros(N)
-        for i in range(self.d):
+        for i in range(d):
             bias_initial_lower_bounds[hidden_node_keys == i] = torch.tensor(data_bounds_list[i][0], dtype=torch.float)
             bias_initial_upper_bounds[hidden_node_keys == i] = torch.tensor(data_bounds_list[i][1], dtype=torch.float)
         
@@ -60,49 +56,29 @@ class Regression_Cubical_Network_One_Nonlinearity(nn.Module):
         self.register_parameter('bias', self.bias)
 
         # define and initialize the unconstrained weights of the neural network, namely the output layers
-        self.layer2 = nn.Linear(self.N, self.num_labels, bias = True)
-        self.output_layer = nn.Linear(self.num_labels, 1, bias = True)
+        self.output_layer = nn.Linear(self.N, 1, bias = True)
 
         # define the activation function of the network
         self.hardtanh1 = nn.Hardtanh(min_val = 0, max_val = 1)
 
         self.flatten = nn.Flatten()
 
+
     def forward(self, x):
-        # create a dictionary that assigns to each dimension i, the column vector of the i-th component of each data point in the batch
-        batch_component_dict = dict()
-        for j in range(0, self.d):
-            # select the j-th component of every data point in the batch and reshape into a column vector
-            batch_component_dict[j] = torch.reshape(x[:, j], (self.batch_size, 1))
-
-        layer1 = torch.ones(1, self.N)
-
-        # interate over dimension
-        for i in range(0, self.d):
-            weights_for_dim_eq_i = torch.zeros(self.N)
-            for k in range(0, self.d):
-                for hidden_node in range(int(k*(self.N/self.d)), int((k+1)*(self.N/self.d))):
-
-                    # the value of weights_for_dim_eq_i is the same up to a constant multiple for nodes of the same node key
-                    weights_for_dim_eq_i[hidden_node] += (self.shared_weight_matrix[k][i] * self.weight_coefficients[hidden_node])
-
-            # z_i keeps track of the value of the hidden nodes of key i for all of the data in the batch
-            z_i = (batch_component_dict[i] * layer1) * weights_for_dim_eq_i
-
-            # hidden_layer_before_hardtanh is a a tensor of shape batch_size x N that gets filled with the value of the hidden nodes for all of the data in the batch
-            if i == 0:
-                hidden_layer_before_hardtanh = z_i
-            else: 
-                hidden_layer_before_hardtanh += z_i
-
-        # the operation hidden_layer_before_hardtanh + self.bias adds the N-th entry of self.bias to each entry of the N-th column of hidden_layer_before_hardtanh
-        layer1_output = self.hardtanh1(hidden_layer_before_hardtanh + self.bias)
-
-        # apply second and final layer weights 
-        output = self.output_layer(self.layer2(layer1_output))
-        if self.threshold_prediction:
-            return torch.clamp(output, min=0.0, max = float(self.num_labels - 1))
         
+        unscaled_weight_matrix = self.shared_weight_matrix.repeat_interleave(self.group_size, dim=0)
+
+        weight_matrix = unscaled_weight_matrix * self.weight_coefficients.view(-1, 1)
+
+        # apply weight matrix and bias vector
+        hidden_layer_before_hardtanh = torch.matmul(x, weight_matrix.T) + self.bias
+        layer1_output = self.hardtanh1(hidden_layer_before_hardtanh)
+
+        # apply final layer weights 
+        output = self.output_layer(layer1_output)
+
+        if self.threshold_prediction:
+            return torch.clamp(output, min=0.0, max = self.threshold)
         else:
             return output
 
